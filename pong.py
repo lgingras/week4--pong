6286e16deec3ed4cd255d6a5b8f23dc187ccfa2f
lgingras <gingras.leanna@gmail.com>
2013-11-05 20:59:54 -0800

    added more lecture examples and started the pong game

---
+++ pong.py
@@ -0,0 +1,79 @@
+# Implementation of classic arcade game Pong
+
+import simplegui
+import random
+
+# initialize globals - pos and vel encode vertical info for paddles
+WIDTH = 600
+HEIGHT = 400       
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+LEFT = False
+RIGHT = True
+
+ball_pos = [WIDTH / 2, HEIGHT / 2]
+vel = [1, 0]
+
+# initialize ball_pos and ball_vel for new bal in middle of table
+# if direction is RIGHT, the ball's velocity is upper right, else upper left
+def spawn_ball(direction):
+    global ball_pos, ball_vel # these are vectors stored as lists
+
+
+# define event handlers
+def new_game():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
+    global score1, score2  # these are ints
+
+def draw(c):
+    global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
+        
+    # draw mid line and gutters
+    c.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    c.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
+    c.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
+        
+    # update ball
+    ball_pos[0] += vel[0]
+    ball_pos[1] += vel[1]
+    
+    # collide and reflect off of left hand side of canvas
+    if ball_pos[0] <= BALL_RADIUS + 8:
+        vel[0] = - vel[0]
+
+    # collide and reflect off of right hand side of canvas
+    if ball_pos[0] >= (WIDTH - 8 ) - BALL_RADIUS:
+        vel[0] = - vel[0]
+                       
+    # draw ball
+    c.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
+    
+    # update paddle's vertical position, keep paddle on the screen
+    
+    # draw paddles
+#    c.draw_polygon(paddle1_pos, [PAD_WIDTH, PAD_HEIGHT], "Blue", "Blue")
+#    c.draw_polygon(paddle2_pos, [PAD_WIDTH, PAD_HEIGHT], "Blue", "Blue")
+    
+    
+    # draw scores
+        
+def keydown(key):
+    global paddle1_vel, paddle2_vel
+   
+def keyup(key):
+    global paddle1_vel, paddle2_vel
+
+
+# create frame
+frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+frame.set_keydown_handler(keydown)
+frame.set_keyup_handler(keyup)
+
+
+# start frame
+new_game()
+frame.start()
